bcf8af4f170e2a161e914154c7aa2c7d93c56eb2
Luiz Henrique <luizheennry@icloud.com>
2025-04-15 20:36:45 -0300

    Atualização completa do projeto: backup, utils e workflow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from delete.delete_data import deletar_periodo
 
-if __name__ == '__main__':
-    tabelas_periodo = ["fato_gross_up","Fato_UR", "analitico_assinaturas", "fato_portabilidade", "fato_mplay"]
-    deletar_periodo(tabelas_periodo, "2025-03")
+if __name__ == "__main__":
+    tabela = "Fato_UR"
+    periodo = "2025-03"
+    deletar_periodo(tabela, periodo)
